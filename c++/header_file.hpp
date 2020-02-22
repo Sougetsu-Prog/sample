c701962e5d7141277f5dc61eb6d900f0b5e98dc1
sougetsu <kazu@sakakibarakazuakinoMacBook-Pro.local>
2020-02-22 11:14:37 +0900

    add "header_file.hpp"

---
+++ c++/header_file.hpp
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE_H   //If HEADER_FILE_H isn't defined, to enable definition to #endif
+#define HEADER_FILE_H   //Define HEADER_FILE_H
+
+void mySwap(int& a, int& b){};
+void mySwap(double& a, double& b){};
+
+#endif
