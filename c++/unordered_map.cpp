966ba8e560dbb3c9ea0e12925a37a2fe2461ada7
sougetsu <kazuxtopazius@gmail.com>
2020-02-23 21:41:50 +0900

    add comments

--- c++/unordered_map.cpp
+++ c++/unordered_map.cpp
@@ -2,18 +2,23 @@
 #include <string>
 #include <unordered_map>
 
+/* Use unordered_map */
 int main() {
     std::unordered_map<std::string, int> dictionary {{"one", 0}, {"two", 2}, {"three", 3}};
     std::cout << dictionary["two"] << std::endl;
 
+    /* Change {"one", 0} 's value 0 to 1 */
     dictionary["one"] = 1;
+    /* Add key&value( {"four", 0} ) pair if not exist */
     std::cout << dictionary["four"] << std::endl; //出力値0
 
-    //検索するだけなら要素は登録されない
+    /* Not register elements if only search */
     if (dictionary.find("five") == dictionary.end()) std::cout << "not found\n";
 
+    /* Add a element */
     dictionary["six"] = 6;
 
+    /* Display a map */
     for(const auto& p : dictionary) {
         std::cout << p.first << ", " << p.second << std::endl;
     }
